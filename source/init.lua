1714d7c25f8dcc6531b9a41d96d3df5c4a745289
Kunologist <jiyikun2002@gmail.com>
2019-07-15 22:24:06 +0800

    Create init.lua

---
+++ source/init.lua
@@ -0,0 +1,12 @@
+-- Build Metadata
+appcode="34"
+template="bottom"
+packagename="com.fanmade.taustation"
+appver="0.0.6.0"
+appname="Tau Station"
+user_permission={
+  [4]	= "WRITE_EXTERNAL_STORAGE" ;
+  [1]	= "ACCESS_NETWORK_STATE" ;
+  [2]	= "ACCESS_WIFI_STATE" ;
+  [3]	= "INTERNET" ;
+  } ;
